19b805ab29f016d4c76b186bca9317a015f3b0a6
Git-Suraj-hub <suraj773714@gmail.com>
2025-01-10 14:49:04 +0530

    Question Of Matrix Multipication

---
+++ Machine Learning/Question1.py
@@ -0,0 +1,6 @@
+import numpy as np
+x = np.array([[0,1,2],[3,4,5],[6,7,8]])
+y = np.array([[-1],[1],[2]])
+print(x)
+print(y)
+print(np.dot(x,y))
---
+++ Machine Learning/Question3.py
@@ -0,0 +1,6 @@
+import numpy as np
+x =np.array([[1,0,0],[0,1,0],[0,0,1]])
+y = np.array([[-1],[1],[2]])
+print(x)
+print(y)
+print(np.dot(x,y))
